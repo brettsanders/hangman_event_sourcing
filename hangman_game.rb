cdc7cdb8db296061e1b892760d80ef14934e0c38
Brett Sanders <brettrsanders@gmail.com>
2018-12-31 12:25:17 -0500

    Bit more structure setup

--- hangman_game.rb
+++ hangman_game.rb
@@ -1,11 +1,14 @@
 require 'securerandom'
+require_relative 'helpers/events'
 
-end_game = false
 
-until end_game == true
-  puts "Welcome to Hangman"
-  puts "Should I end the game? (Y/N)"
+# Game Loop
+# end_game = false
 
-  end_game_input = gets.chomp
-  end_game = "Y" ? true : false
-end
+# until end_game == true
+#   puts "Welcome to Hangman"
+#   puts "Should I end the game? (Y/N)"
+
+#   end_game_input = gets.chomp
+#   end_game == "Y" ? true : false
+# end
---
+++ helpers/events.rb
@@ -0,0 +1,2 @@
+# TODO: Store Events under Folders per player
+Dir.mkdir("_events/player1") unless File.exists?("_events/player1")
